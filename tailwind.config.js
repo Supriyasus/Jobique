5e6294cce165089a84dd41ffdcb0c45c280c6e2f
Supriya <supriyasrivastava2004@gmail.com>
2025-03-29 02:59:34 +0530

    Changes in gitignore

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./frontend/**/*.html", "./frontend/**/*.js"],
+    content: [
+      "./frontend/*.html",
+      "./frontend/*.js",
+      "./frontend/src/*.css"
+    ],
     theme: {
       extend: {},
     },
     plugins: [],
-  };
-  
+  }
